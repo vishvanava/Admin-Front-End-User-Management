54d09aa77344bd8c709e60db19f81cb5a8130824
vishvanava <vishva.navanjana@clouda.io>
2023-01-13 11:10:13 +0530

    resolve chakra ui issue

--- admin-front-user-management/pages/_app.tsx
+++ admin-front-user-management/pages/_app.tsx
@@ -1,9 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-      <Component {...pageProps} />
+    <ChakraProvider>
+    <Component {...pageProps} />
+    </ChakraProvider>
   ); 
 }
 
