172e15eebacd476ca495547d5db18fc60e138711
vishvanava <vishva.navanjana@clouda.io>
2023-01-13 12:25:48 +0530

    docker file added and ecr repo created

--- admin-front-user-management/pages/index.tsx
+++ admin-front-user-management/pages/index.tsx
@@ -1,10 +1,10 @@
 import Head from 'next/head'
-import { Inter } from '@next/font/google'
+// import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 // import Link from 'next/link'
 import Navbar from '../components/layout/navbar/navbar'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
